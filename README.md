1be88e86ca5ee3c720d9dbbb2ad47388dfcaf5d1
Patrick O'Dea <po39dea@gmail.com>
2023-09-24 21:16:55 -0600

    Created function to write reame file and function to initialize app

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Readme-File-Generator
+
+# undefined
+        
+## Description
+g
+        
+## Table of Contents
+undefined
+        
+## Installation
+undefined
+        
+## Usage
+g
+        
+## License
+g
+        
+## Contributing
+undefined
+        
+## Tests
+g
+        
+## Questions
+g
--- index.mjs
+++ index.mjs
@@ -5,8 +5,7 @@ import inquirer from 'inquirer';
 //import generateMarkdown from './utils/generateMarkdown';
 
 // TODO: Create an array of questions for user input
-inquirer
-    .prompt([
+const questions = [
 {
     type: "input",
     message: "What is the title of your project?",
@@ -52,10 +51,50 @@ inquirer
     message: "Remaining questions you may have?",
     name: "questions",
 },
-    ])
+];
 // TODO: Create a function to write README file
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`File ${fileName} created successfully.`);
+    }
+    });
+}
 // TODO: Create a function to initialize app
-
+function init() {
+    // Call inquirer.prompt to collect user input
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = `
+# ${answers.projectTitle}
+        
+## Description
+${answers.description}
+        
+## Table of Contents
+${answers.tableofcontents}
+        
+## Installation
+${answers.installation}
+        
+## Usage
+${answers.usage}
+        
+## License
+${answers.license}
+        
+## Contributing
+${answers.contributing}
+        
+## Tests
+${answers.tests}
+        
+## Questions
+${answers.questions}
+`;
+      writeToFile('README.md', readmeContent); // Replace 'generateMarkdown' with your actual function
+    });
+}
 // Function call to initialize app
 init();
