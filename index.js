a840548c988906a05e8b64f59a1faadb60a863d6
Patrick O'Dea <po39dea@gmail.com>
2023-09-24 20:09:03 -0600

    Creating File Structure

---
+++ index.js
@@ -0,0 +1,10 @@
+// TODO: Include packages needed for this application
+
+// TODO: Include packages needed for this application
+
+// TODO: Create a function to write README file
+
+// TODO: Create a function to initialize app
+
+// Function call to initialize app
+init();
